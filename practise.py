b0e5535f113da4aa52f11f5a47eeae55375311b4
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-13 23:15:54 +0530

    Add files via upload

---
+++ practise.py
@@ -0,0 +1,14 @@
+'''
+DATE=13TH NOV 2022
+DAY= sunday
+TOPIC= STRINGS 2
+AUTHOR= KIRANMAI
+'''
+#a=int()
+#print(a,type(a)
+#print("the value of a is " +a))
+#print("the value of a is ",a)
+
+b=float(input("enter your height:: "))
+print(b,type(b))
+
